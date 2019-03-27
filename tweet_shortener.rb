f0d18230e40e081a5e7d5310b1a09963c0fefc27
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-27 14:06:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,10 +39,17 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweets)
+    tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
 
 def selective_tweet_shortener
   if tweet.length > 140
-    
+    word_substituter(tweet)
+  else
+    tweet
+  end
 end
 
 def shortened_tweet_truncator()
