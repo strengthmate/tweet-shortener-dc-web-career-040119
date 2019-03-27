2230c9967723e7ed72db143283477801570d9b5d
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-27 13:58:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -10,6 +10,19 @@ def dictionary
   "and" => "&"
 }
 end
+def dictionary
+dictionary = {
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
 
 #Attempred solution
 # def word_substituter (tweet)
