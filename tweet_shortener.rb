02508a9226fcbb2cc00e637d953bc70a16641b9f
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-27 13:58:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,27 +2,18 @@
 def dictionary 
   dictionary = {
   "hello" => 'hi',
-  "to, two, too" => '2',
-  "for, four" => '4',
+  "to" => '2', 
+  "two" => '2',
+  "too" => '2',
+  "for => '4',
+  "four" => '4',
   'be' => 'b',
   'you' => 'u',
   "at" => "@",
   "and" => "&"
 }
 end
-def dictionary
-dictionary = {
-  "too" => "2",
-  "to" => "2",
-  "two" =>"2",
-  "four" => "4",
-  "for" => "4",
-  "be" => "b",
-  "you" => "u",
-  "at" => "@",
-  "and" => "&"
-}
-end
+
 
 #Attempred solution
 # def word_substituter (tweet)
