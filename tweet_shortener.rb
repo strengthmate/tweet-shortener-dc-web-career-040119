8a40cafce9c05c4d7fb4a09f5c3a97df3f25a05e
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-27 13:59:53 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,3 +28,12 @@ end
 # end
 
 #Actual Solution
+def word_substituter(tweet)
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
