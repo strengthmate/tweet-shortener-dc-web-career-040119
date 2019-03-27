2395450a69df58e133bbb5475872a6b13147fe3e
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-27 13:52:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,7 +19,18 @@ def word_substituter (tweet)
       if words = key
         words = value
       end
-        return arr.join("")
     end
+            return arr.join("")
   end
 end
+
+
+# def word_substituter(tweet)
+#   tweet.split.collect do |word|
+#     if dictionary.keys.include?(word.downcase)
+#       word = dictionary[word.downcase]
+#     else
+#       word
+#     end
+#   end.join(" ")
+# end
