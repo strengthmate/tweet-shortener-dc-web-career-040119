8ded858a88c99bcd8075697be78263c5b0949467
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-27 13:47:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def dictionary 
+  dictionary_hash = {
+  "hello" => 'hi',
+  "to, two, too" => '2',
+  "for, four" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@",
+  "and" => "&"
+}
+
+end
+
+def word_substituter (tweet)
+  a = tweet.split("")
+  l = a.size
+  dictionary_hash.each do |key, value|
+    for words in 0..l-1
+      if a[words] = key
+        a[words] = value
+      end
+        return a.join("")
+    end
+  end
+end
